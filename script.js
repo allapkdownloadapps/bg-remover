330c405646f9d82fe8f0f4e2a6015f626bf616d9
allapkdownloadapps <ayankhanofficial2009@gmail.com>
2026-07-27 23:10:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let upload = document.getElementById("upload");
 let imagePreview = document.getElementById("imagePreview");
+let download = document.getElementById("download");
+
+let selectedFile;
 
 
 upload.onchange = function(){
 
-    let file = upload.files[0];
+    selectedFile = upload.files[0];
 
-    if(file){
+    if(selectedFile){
 
-        imagePreview.src = URL.createObjectURL(file);
+        imagePreview.src = URL.createObjectURL(selectedFile);
 
     }
 
@@ -16,10 +19,46 @@ upload.onchange = function(){
 
 
 
-function removeBG(){
+async function removeBG(){
 
-    alert(
-    "Background remover AI connect karna baki hai. Next step me API add karenge."
+    if(!selectedFile){
+        alert("Pehle image upload karo");
+        return;
+    }
+
+
+    let formData = new FormData();
+
+    formData.append("image_file", selectedFile);
+    formData.append("size","auto");
+
+
+    let response = await fetch(
+        "https://api.remove.bg/v1.0/removebg",
+        {
+            method:"POST",
+
+            headers:{
+                "X-Api-Key":"aNmVoeoS3bxykDa6aKFo6dn4"
+            },
+
+            body:formData
+        }
     );
+
+
+    let blob = await response.blob();
+
+
+    let url = URL.createObjectURL(blob);
+
+
+    imagePreview.src = url;
+
+
+    download.href = url;
+    download.style.display="block";
+    download.innerHTML="Download PNG";
+
 
 }
